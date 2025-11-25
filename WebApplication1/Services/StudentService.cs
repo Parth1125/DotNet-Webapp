a9e66da10d05c332e0d503885bdaa12b9740f415
Parth1125 <pgera326@gmail.com>
2025-11-25 10:47:41 +0530

    added logic to add details in database

--- WebApplication1/Services/StudentService.cs
+++ WebApplication1/Services/StudentService.cs
@@ -1,37 +1,47 @@
 using WebApplication1.Models;
+using WebApplication1.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Services
 {
     public class StudentService: IStudentService
     {
-        private static List<Student> students = new List<Student>();
+        // private static List<Student> students = new List<Student>();
+        private readonly StudentDbContext _db;
+        public StudentService(StudentDbContext db)
+        {
+            _db=db;
+
+        }
         public Student AddStudent(Student s)
         {
-            students.Add(s);
+            _db.Students.Add(s);
+            _db.SaveChanges();
             return s;
         }
         public List<Student> GetAll()
         {
-            return students;
+            return _db.Students.ToList();
         }
         public Student? GetByName(string name)
         {
-            var student = students.FirstOrDefault(x=>x.Name== name);
+            var student = _db.Students.FirstOrDefault(x=>x.Name== name);
             return student;
         }
         public Student? UpdateStudent(string name, Student updated)
         {
-            var student = students.FirstOrDefault(x=>x.Name == name);
+            var student = _db.Students.FirstOrDefault(x=>x.Name == name);
             if(student == null) return null;
             student.Name= updated.Name;
             student.Age = updated.Age;
+            _db.SaveChanges();
             return student;
         }
         public bool Delete(string name)
         {
-            var s = students.FirstOrDefault(x=>x.Name == name);
+            var s = _db.Students.FirstOrDefault(x=>x.Name == name);
             if(s== null) return false;
-            students.Remove(s);
+            _db.Students.Remove(s);
             return true;
         }
     }
